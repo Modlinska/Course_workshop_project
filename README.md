9fb75cacdb217a9de84adb382574cfc2736c4f4c
Karolina Modlinska <karolina.rzeznik@gmail.com>
2019-04-13 20:23:25 +0200

    change submenu

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Course_workshop_project
 A project created during coding bootcamp
+
+There is submenu in navigation. It's appears after click.
+
+The checkbox is personalized with css transition
+
--- js/script.js
+++ js/script.js
@@ -2,22 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
 // Menu pojawia się i znika po najechaniu i zabraniu myszki
     var navLi = document.querySelectorAll(" nav > ul > li");
     console.log(navLi);
+    // for (var i = 0; i < navLi.length; i++) {
+    //     //     navLi[i].addEventListener('mouseover', function () {
+    //     //         if (this.querySelector('.dropdown') != null) {
+    //     //             this.querySelector('.dropdown').style.display = 'block';
+    //     //
+    //     //         }
+    //     //     })
+    //     // }
+    //     //
+    //     // for (var i = 0; i < navLi.length; i++) {
+    //     //     navLi[i].addEventListener('mouseout', function () {
+    //     //         if (this.querySelector('.dropdown') != null) {
+    //     //             this.querySelector('.dropdown').style.display = 'none';
+    //     //
+    //     //         }
+    //     //     })
+    //     // }
+// Menu pojawia sie i znika po kliknięciu:
     for (var i = 0; i < navLi.length; i++) {
-        navLi[i].addEventListener('mouseover', function () {
+        navLi[i].addEventListener('click', function () {
             if (this.querySelector('.dropdown') != null) {
-                this.querySelector('.dropdown').style.display = 'block';
+                this.querySelector('.dropdown').classList.toggle("visible");
             }
         })
     }
-
-        for (var i = 0; i < navLi.length; i++) {
-            navLi[i].addEventListener('mouseout', function () {
-              if (this.querySelector('.dropdown') != null) {
-                  this.querySelector('.dropdown').style.display = 'none';
-              }
-            })
-        }
-
-
 
 });
